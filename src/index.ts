c399957532a825a458ee322a475f94278c8e0a85
Elias <eliasurenagarcia05@gmail.com>
2025-08-29 14:11:09 +0200

    Configuracion+base de datos MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { Router } from 'express'
+import colors from 'colors'
 import server from './server'
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
-server.listen(port, () => { 
-    console.log('Servidor Funcionando en el puerto: ', port)
+server.listen(port, () => {
+    console.log(colors.bgBlack.magenta.italic('Servidor Funcionando en el puerto: '), port)
 })
-
--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,14 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.post('/auth/register', (req, res) =>{
+router.get('/', (req, res) => {
+    console.log('GET / ejecutado')
+    res.send('Hola mundo desde GET /')
+})
+
+router.post('/auth/register', (req, res) => {
     console.log('Desde register')
+    res.status(200).json({ message: 'Registro recibido' })
 })
 
 export default router
